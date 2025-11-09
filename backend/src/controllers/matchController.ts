ec05eb378839e7a9a88229ffa10bf24024a94623
Nathan Cloutier <nathanc4571@gmail.com>
2025-11-08 19:06:25 -0500

    Basic join, leave, and start match functionality

--- backend/src/controllers/matchController.ts
+++ backend/src/controllers/matchController.ts
@@ -5,20 +5,22 @@ export const createMatch = async (req: any, res: any) => {
   const user = req.user as User;
 
   try {
-    const ongoingMatches = await prisma.match.findMany({
+    const matches = await prisma.match.findMany({
       where: {
         players: {
           some: {
             id: user.id,
           },
         },
-        status: "ongoing",
       },
     });
-    if (ongoingMatches.length > 0) {
+    const nonCompletedMatches = matches.filter(
+      (match) => match.status !== "completed"
+    );
+    if (nonCompletedMatches.length > 0) {
       return res
         .status(400)
-        .json({ message: "User already has an ongoing match" });
+        .json({ message: "User already has an ongoing or pending match" });
     }
     const newMatch = await prisma.match.create({
       data: {
@@ -53,14 +55,149 @@ export const getAllMatches = async (req: any, res: any) => {
         username: player.username,
       })),
     }));
-    return res
-      .status(200)
-      .json({
-        matches: removedUnnecessaryFieldsMatches,
-        message: "Matches fetched successfully",
-      });
+    return res.status(200).json({
+      matches: removedUnnecessaryFieldsMatches,
+      message: "Matches fetched successfully",
+    });
   } catch (error) {
     console.error("Error fetching matches:", error);
     return res.status(500).json({ message: "Failed to fetch matches" });
+  }
+};
+
+export const joinMatch = async (user: User, matchId: number) => {
+  try {
+    const match = await prisma.match.findUnique({
+      where: { id: matchId },
+      include: { players: true },
+    });
+    if (!match) {
+      throw new Error("Match not found");
+    }
+    if (match.status !== "pending") {
+      throw new Error("Cannot join a match that is not pending");
+    }
+    const matches = await prisma.match.findMany({
+      where: {
+        players: {
+          some: {
+            id: user.id,
+          },
+        },
+      },
+    });
+    const nonCompletedMatches = matches.filter(
+      (match) => match.status !== "completed"
+    );
+    if (nonCompletedMatches.length > 0) {
+      throw new Error("User already has an ongoing or pending match");
+    }
+    const newMatch = await prisma.match.update({
+      where: { id: matchId },
+      data: {
+        players: {
+          connect: { id: user.id },
+        },
+      },
+      include: { players: true, map: true },
+    });
+    const playerFilteredMatch = {
+      id: newMatch.id,
+      players: newMatch.players.map((player) => ({
+        id: player.id,
+        username: player.username,
+      })),
+      winnerId: newMatch.winnerId,
+      status: newMatch.status,
+      map: newMatch.map,
+      scores: newMatch.scores,
+    };
+    return playerFilteredMatch;
+  } catch (error) {
+    console.error("Error joining match:", error);
+    throw new Error("Failed to join match");
+  }
+};
+
+export const leaveMatch = async (user: User, matchId: number) => {
+  try {
+    const match = await prisma.match.findUnique({
+      where: { id: matchId },
+      include: { players: true },
+    });
+    if (!match) {
+      throw new Error("Match not found");
+    }
+    if (!match.players.find((player) => player.id === user.id)) {
+      throw new Error("User is not part of this match");
+    }
+    const newMatch = await prisma.match.update({
+      where: { id: matchId },
+      data: {
+        players: {
+          disconnect: { id: user.id },
+        },
+      },
+      include: { players: true, map: true },
+    });
+    const playerFilteredMatch = {
+      id: newMatch.id,
+      players: newMatch.players.map((player) => ({
+        id: player.id,
+        username: player.username,
+      })),
+      winnerId: newMatch.winnerId,
+      status: newMatch.status,
+      map: newMatch.map,
+      scores: newMatch.scores,
+    };
+    return playerFilteredMatch;
+  } catch (error) {
+    console.error("Error leaving match:", error);
+    throw new Error("Failed to leave match");
+  }
+};
+
+export const startMatch = async (user: User, matchId: number) => {
+  try {
+    const match = await prisma.match.findUnique({
+      where: { id: matchId },
+      include: { players: true },
+    });
+    if (!match) {
+      throw new Error("Match not found");
+    }
+    if (match.status !== "pending") {
+      throw new Error("Cannot start a match that is not pending");
+    }
+    if (!match.players.some((player) => player.id === user.id)) {
+      throw new Error("User is not part of this match");
+    }
+    if (match.players.length < 2) {
+      throw new Error("Not enough players to start the match");
+    }
+    // TODO: Generate a map for the match.
+    const updatedMatch = await prisma.match.update({
+      where: { id: matchId },
+      data: {
+        status: "ongoing",
+      },
+      include: { players: true, map: true },
+    });
+    const playerFilteredMatch = {
+      id: updatedMatch.id,
+      players: updatedMatch.players.map((player) => ({
+        id: player.id,
+        username: player.username,
+      })),
+      winnerId: updatedMatch.winnerId,
+      status: updatedMatch.status,
+      map: updatedMatch.map,
+      scores: updatedMatch.scores,
+    };
+    return playerFilteredMatch;
+  } catch (error) {
+    console.error("Error starting match:", error);
+    throw new Error("Failed to start match");
   }
 };
--- backend/src/socket/matchSocket.ts
+++ backend/src/socket/matchSocket.ts
@@ -2,6 +2,11 @@ import { Server } from "socket.io";
 import { parse } from "cookie";
 import authenticate from "../utils/authenticate";
 import { User } from "@prisma/client";
+import {
+  joinMatch,
+  leaveMatch,
+  startMatch,
+} from "../controllers/matchController";
 
 const matchSocket = (io: Server) => {
   io.use(async (socket, next) => {
@@ -25,6 +30,24 @@ const matchSocket = (io: Server) => {
   io.on("connection", (socket) => {
     const user = (socket as any).user as User;
     console.log(`User connected: ${user.username}`);
+
+    socket.on("joinMatch", async (matchId: number) => {
+      const match = await joinMatch(user, matchId);
+      socket.join(`match_${match.id}`);
+      io.to(`match_${match.id}`).emit("playerJoined", { match });
+      socket.emit("joinedMatch", { match });
+    });
+
+    socket.on("leaveMatch", async (matchId: number) => {
+      const match = await leaveMatch(user, matchId);
+      socket.leave(`match_${matchId}`);
+      io.to(`match_${matchId}`).emit("playerLeft", { match });
+      socket.emit("leftMatch");
+    });
+
+    socket.on("startMatch", async (matchId: number) => {
+      const match = await startMatch(user, matchId);
+    });
 
     socket.on("disconnect", () => {
       console.log(`User disconnected: ${user.username}`);
